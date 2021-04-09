30846193990e056040746dfa5320e28bd9e88de0
Scott Russell <srussell1383@gmail.com>
2021-04-09 00:06:33 -0400

    Made a minor change so that Heroku would pick it up.

--- src/Components/AboutMe/DescriptionWidget.js
+++ src/Components/AboutMe/DescriptionWidget.js
@@ -1,8 +1,8 @@
-import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 
 
-const descriptionText = "Accomplished Software Developer that brings more to the table than just clean, resilient, and efficient code.";
+const descriptionText = `Accomplished Software Developer that brings more to the 
+table than just clean, resilient, and efficient code.`;
 
 
 const useStyles = makeStyles({
